ba3c271ca7d2c433ee057d77b55c05cd4bde9586
Admin stagiaire <Admin stagiaire@a4bb6d7551b5>
2021-01-19 15:29:04 +0100

    installation entity Framework

---
+++ Shop.DataAcces.Sql/MyContext.cs
@@ -0,0 +1,30 @@
+﻿using Shop.Core.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Shop.DataAcces.Sql
+{
+    public class MyContext : DbContext
+    {
+        // Votre contexte a été configuré pour utiliser une chaîne de connexion « MyContext » du fichier 
+        // de configuration de votre application (App.config ou Web.config). Par défaut, cette chaîne de connexion cible 
+        // la base de données « Shop.DataAcces.Sql.MyContext » sur votre instance LocalDb. 
+        // 
+        // Pour cibler une autre base de données et/ou un autre fournisseur de base de données, modifiez 
+        // la chaîne de connexion « MyContext » dans le fichier de configuration de l'application.
+        public MyContext()
+            : base("name=MyContext")
+        {
+        }
+
+       DbSet<Product> products { get; set; }
+        DbSet<ProductCategory> productCategories { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
