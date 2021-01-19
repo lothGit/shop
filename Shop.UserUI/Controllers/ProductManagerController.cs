cf170ab3b8f7a2ea9b9eb152813ac2e1a5db6926
Admin stagiaire <Admin stagiaire@a4bb6d7551b5>
2021-01-19 10:57:32 +0100

    Product controller and view tested

---
+++ Shop.UserUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,150 @@
+﻿using Shop.Core.Models;
+using Shop.DataAcces.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.UserUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductRepository context;
+
+        public ProductManagerController()
+        {
+            context = new ProductRepository();
+        }
+
+
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+        public ActionResult Create()
+        {
+            Product p = new Product();
+            return View(p);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                Product p = context.FindById(id);
+                if (p == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    return View(p);
+                }
+            }
+            catch (Exception)
+            {
+                return HttpNotFound();
+            }
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product, int id)
+        {
+
+            try
+            {
+                Product prodToEdit = context.FindById(id);
+                if (prodToEdit == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+
+                    if (ModelState.IsValid)
+                    {
+                        return View(product);
+                    }
+                    else
+                    {
+                        //context.Update(product);
+                        prodToEdit.Name = product.Name;
+                        prodToEdit.Description = product.Description;
+                        prodToEdit.Price = product.Price;
+                        prodToEdit.Image = product.Image;
+                        prodToEdit.Category = product.Category;
+                        context.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                return HttpNotFound();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                Product p = context.FindById(id);
+                if (p == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    return View(p);
+                }
+            }
+            catch (Exception)
+            {
+                return HttpNotFound();
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+            try
+            {
+                Product p = context.FindById(id);
+                if (p == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    context.Delete(id);
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
